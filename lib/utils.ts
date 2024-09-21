d53bb1218321f09584cc07175dc58ffec20316d6
ppaanngggg <hantian.pang@gmail.com>
2024-09-21 22:12:39 +0800

    fix fetch when building

--- lib/utils.ts
+++ lib/utils.ts
@@ -12,12 +12,8 @@ export const getURL = (path: string = "") => {
     process?.env?.NEXT_PUBLIC_SITE_URL &&
     process.env.NEXT_PUBLIC_SITE_URL.trim() !== ""
       ? process.env.NEXT_PUBLIC_SITE_URL
-      : // If not set, check for NEXT_PUBLIC_VERCEL_URL, which is automatically set by Vercel.
-        process?.env?.NEXT_PUBLIC_VERCEL_URL &&
-          process.env.NEXT_PUBLIC_VERCEL_URL.trim() !== ""
-        ? process.env.NEXT_PUBLIC_VERCEL_URL
-        : // If neither is set, default to localhost for local development.
-          "http://localhost:3000/";
+      : // If neither is set, default to localhost for local development.
+        "http://localhost:3000/";
 
   // Trim the URL and remove trailing slash if exists.
   url = url.replace(/\/+$/, "");
