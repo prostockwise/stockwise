a8ce80d7dc01e4ddacd009004033e013f73501c0
ht.pang <hantian.pang@gmail.com>
2024-09-24 21:23:59 +0800

    fix news page

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,6 +1,8 @@
 import { todayDate } from "@/lib/utils";
 import { redirect } from "next/navigation";
 
+export const revalidate = 60;
+
 export default async function NewsPage() {
   const today = todayDate();
   redirect(`/news/${today}`);
