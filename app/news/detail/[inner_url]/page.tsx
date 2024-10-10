93c63737f155294adca51307df7e96e115fb8841
ppaanngggg <hantian.pang@gmail.com>
2024-10-10 21:59:29 +0800

    add home to news detail page

--- app/news/detail/[inner_url]/page.tsx
+++ app/news/detail/[inner_url]/page.tsx
@@ -49,6 +49,15 @@ async function Navbar({ date }: { date: string }) {
         <ChevronLeft className="h-6 w-6 mr-2" />
         <span className="text-lg font-bold">Back to News</span>
       </Link>
+      <nav className="ml-auto flex gap-4 sm:gap-6">
+        <Link
+          className="text-sm font-medium hover:underline underline-offset-4"
+          href="/"
+          prefetch={false}
+        >
+          Home
+        </Link>
+      </nav>
     </header>
   );
 }
