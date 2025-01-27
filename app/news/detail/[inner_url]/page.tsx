402850c46eac4eac5cf0e7d14ce267fd8a1dd8d3
Stockwise <prostockwise@gmail.com>
2025-01-27 21:09:24 +0800

    fix old detail page

--- app/news/detail/[inner_url]/page.tsx
+++ app/news/detail/[inner_url]/page.tsx
@@ -1,23 +1,19 @@
-import { getURL } from "@/lib/utils";
 import { permanentRedirect, redirect } from "next/navigation";
-import { Tables } from "@/types_db";
+import { fetchNewsDetail } from "@/lib/news";
 
 export default async function DetailNewsPage({
   params,
-  searchParams,
 }: {
   params: { inner_url: string };
-  searchParams: { refer: string | undefined };
 }) {
   // fetch news
-  const resp = await fetch(getURL(`/api/news/detail/${params.inner_url}`));
-  if (!resp.ok) {
+  let news;
+  try {
+    news = await fetchNewsDetail(params.inner_url);
+  } catch (e) {
+    console.log(e);
     redirect("/");
   }
-  const news: Tables<"news"> = await resp.json();
   let newUrl = `/news/${news.date}/${news.inner_url}`;
-  if (searchParams.refer) {
-    newUrl += `?refer=${encodeURI(searchParams.refer)}`;
-  }
   permanentRedirect(newUrl);
 }
